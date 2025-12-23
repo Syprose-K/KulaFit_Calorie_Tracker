f2e4f39d7cf57219b9a1bcc9ebb1bfaab7182c92
syprose-k <kichesyprose@gmail.com>
2025-12-23 20:00:41 +0300

    meals urlpatterns for log_wheight and dashboard

--- kulafit/meals/urls.py
+++ kulafit/meals/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import log_meal
+from .views import log_meal, log_weight, dashboard
 
 urlpatterns = [
     path('log/', log_meal, name='log-meal'),
+    path('weight/', log_weight, name='log-weight'),
+    path('dashboard/', dashboard, name='dashboard'),
 ]
